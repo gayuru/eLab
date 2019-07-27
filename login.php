4f5787239811f64179a8e8d55a5ce294d89b9b0b
Sogyal <s3692351@student.rmit.edu.au>
2019-07-27 21:07:14 +1000

    Updated form

--- login.php
+++ login.php
@@ -36,7 +36,7 @@
 	
 	<div class="container-login100" style="background-image: url('img/bg-01.jpg');">
 		<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-			<form class="login100-form validate-form">
+			<form action="php/login.php" method="post" class="login100-form validate-form">
 				<span class="login100-form-title p-b-37">
 					<img src="img/eLab.png">
 					Sign In
