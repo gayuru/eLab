0a1d14348537110c779f9c463c405c63429cbf7f
Edward Kahiro Kuo <kahirokuo@gmail.com>
2019-07-27 20:49:40 +1000

    moved link_rel to a separate php file

--- entry.php
+++ entry.php
@@ -9,16 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up Form</title>
     <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="vendor/vendors/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="vendor/vendors/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="vendor/vendors/themify-icons/themify-icons.css">
-    <link rel="stylesheet" href="vendor/vendors/linericon/style.css">
-    <link rel="stylesheet" href="vendor/vendors/owl-carousel/owl.theme.default.min.css">
-    <link rel="stylesheet" href="vendor/vendors/owl-carousel/owl.carousel.min.css">
-    <link rel="stylesheet" href="vendor/vendors/flat-icon/font/flaticon.css">
-    <link rel="stylesheet" href="vendor/vendors/nice-select/nice-select.css">
-    <link rel="stylesheet" href="css/style.css">
+    <?php include('link_rel.php'); ?>
     <link rel="stylesheet" href="css/signup.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>eLab - Home</title>
-	<link rel="icon" href="img/Fevicon.png" type="image/png">
-
-  <link rel="stylesheet" href="vendor/vendors/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="vendor/vendors/fontawesome/css/all.min.css">
-  <link rel="stylesheet" href="vendor/vendors/themify-icons/themify-icons.css">
-  <link rel="stylesheet" href="vendor/vendors/linericon/style.css">
-  <link rel="stylesheet" href="vendor/vendors/owl-carousel/owl.theme.default.min.css">
-  <link rel="stylesheet" href="vendor/vendors/owl-carousel/owl.carousel.min.css">
-  <link rel="stylesheet" href="vendor/vendors/flat-icon/font/flaticon.css">
-  <link rel="stylesheet" href="vendor/vendors/nice-select/nice-select.css">
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>eLab - Home</title>
+    <?php include('link_rel.php'); ?>
 </head>
 <body class="bg-shape">
 
