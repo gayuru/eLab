7eb2d5f1a8d83c11a57a652b5d02c06d5f46d2e4
Gayuru <ggayurugunawardana@gmail.com>
2019-07-30 09:58:36 +1000

    Change 2.0

--- login.php
+++ login.php
@@ -36,7 +36,7 @@
 
 	<div class="container-login100" style="background-image: url('img/bg-01.jpg');">
 		<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-			<form action="quixlab-master/index.php" method="post" class="login100-form validate-form">
+			<form action="dashboard/index.php" method="post" class="login100-form validate-form">
 				<span class="login100-form-title p-b-37">
 					<img src="img/eLab.png">
 					Sign In
